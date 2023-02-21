5a4e63a7f7c12cbee096bc74060f4a0663265e3f
Eduardo Andrade <edu2andrade@gmail.com>
2023-02-21 20:51:10 +0000

    feat: inputs to set the card heigth and width implemented

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import "./style.css";
-const container = document.querySelector(".container");
+const container = document.querySelector(".card-container");
 const button = document.querySelector("button");
+const form = document.querySelector("form");
+const card = document.querySelector(".card");
 
 const suit = ["heart", "diamond", "club", "spades"];
 const number = [
@@ -32,14 +34,30 @@ const generateRandomCard = () => {
   `);
 };
 
+const changeCardSizes = formData => {
+  const height = formData.target[0].value;
+  const width = formData.target[1].value;
+
+  return (container.innerHTML = `
+    <div class="card ${getElement(
+      suit
+    )}" style="height: ${height}rem; width: ${width}rem">
+      <span class="number">${getElement(number)}</span>
+    </div>
+  `);
+};
+
 window.onload = () => {
   generateRandomCard();
 
   setInterval(() => {
     generateRandomCard();
-  }, 2000);
+  }, 10000);
 };
 
-button.addEventListener("click", () => {
-  generateRandomCard();
+button.addEventListener("click", generateRandomCard);
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  changeCardSizes(e);
 });
